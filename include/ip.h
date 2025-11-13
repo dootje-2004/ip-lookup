4eb233c56d777e59d27e3f9a8fbe5044a177b60e
Aldo Verlinde <aldo.verlinde@gmail.com>
2025-11-13 22:54:45 +0100

    First successful test

---
+++ include/ip.h
@@ -0,0 +1,24 @@
+#ifndef IP_H_
+#define IP_H_
+
+#include <stdint.h>
+
+enum ip_t {
+    IPv4, IPv6
+};
+
+typedef struct {
+    uint32_t ip;
+    uint8_t umask;
+} ipv4_t;
+
+typedef struct {
+    uint64_t group[8];
+    uint8_t umask;
+} ipv6_t;
+
+ipv4_t read_ipv4(char*);
+
+ipv6_t read_ipv6(char*);
+
+#endif
