ac25f00ba954d2347a77f037da10259919e5a8f7
Aldo Verlinde <aldo.verlinde@gmail.com>
2025-11-14 22:28:11 +0100

    First IPv6 test passes

--- src/ip.c
+++ src/ip.c
@@ -14,7 +14,7 @@ ipv4_t read_ipv4(const char *ip)
     uint8_t g = 1;  // group index
     uint8_t b = 0;  // position in group substring
     
-    ipv4.mask = 32;
+    ipv4.ps = 32;
     
     while (1)
     {
@@ -82,21 +82,21 @@ ipv4_t read_ipv4(const char *ip)
                     }
                     else
                     {
-                        fprintf(stderr, "IPv4 %s: invalid character '%c' (%d) in mask\n", ip, c, c);
+                        fprintf(stderr, "IPv4 %s: invalid character '%c' (%d) in ps\n", ip, c, c);
                         goto error_handler;
                     }
                 }
                 if (d == 0)
                 {
-                    fprintf(stderr, "IPv4 string %s has a zero-bit mask\n", ip);
+                    fprintf(stderr, "IPv4 string %s has a zero-bit ps\n", ip);
                     goto error_handler;
                 }
                 if (d > 32)
                 {
-                    fprintf(stderr, "IPv4 string %s has a mask of more than 32 bits: %d\n", ip, d);
+                    fprintf(stderr, "IPv4 string %s has a ps of more than 32 bits: %d\n", ip, d);
                     goto error_handler;
                 }
-                ipv4.mask = d;
+                ipv4.ps = d;
                 goto final_check;
             default:
                 fprintf(stderr, "IPv4 string %s has an invalid character '%c' (%d) at position %d\n", ip, c, c, p-1);
@@ -113,24 +113,75 @@ final_check:
 
 error_handler:
     ipv4.ip = 0;
-    ipv4.mask = 0;
+    ipv4.ps = 0;
     return ipv4;
 }
 
 ipv6_t read_ipv6(const char *ip)
 {
     ipv6_t ipv6;    // IPv6 address
-    // char c;         // ip character being evaluated
-    // uint8_t p = 0;  // position in ip string
-    // int16_t d = -1; // decimal group value
-    uint8_t g;      // group index
-    // uint8_t b = 0;  // position in group substring
+    char c;         // ip character being evaluated
+    uint8_t p = 0;  // position in ip string
+    uint8_t g = 0;  // group index
+    uint8_t b = 0;  // position in group substring
     
     for (g = 0; g < 8; g++)
     {
-        ipv6.group[g] = -1;
+        ipv6.ip[g] = 0;
     }
-    ipv6.mask = 128;
+    ipv6.ps = 128;
 
+    g = 0;
+    while (1)
+    {
+        c = ip[p++];
+        switch (c)
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                ipv6.ip[g] = (ipv6.ip[g] << 4) + c - '0';
+                b++;
+                break;
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+                ipv6.ip[g] = (ipv6.ip[g] << 4) + c - 'a' + 10;
+                b++;
+                break;
+            case ':':
+                b = 0;
+                g++;
+                break;
+            case '/':
+            case '.':
+            case '\0':
+                goto final_check;
+            default:
+                fprintf(stderr, "IPv6 string %s has an invalid character '%c' (%d) at position %d\n", ip, c, c, p-1);
+                goto error_handler;
+        }
+    }
+
+final_check:
+    // TODO: Check number of groups, expand ::
+    return ipv6;
+
+error_handler:
+    for (g = 0; g < 8; g++)
+    {
+        ipv6.ip[g] = 0;
+    }
+    ipv6.ps = 0;
     return ipv6;
 }
--- test/ipv4.cpp
+++ test/ipv4.cpp
@@ -9,7 +9,7 @@ TEST(IPv4Suite, ValidIPv4Constant)
 {
     ipv4_t ip = read_ipv4("1.2.3.4");
     EXPECT_EQ(ip.ip, 16909060);
-    EXPECT_EQ(ip.mask, 32);
+    EXPECT_EQ(ip.ps, 32);
 }
 
 TEST(IPv4Suite, ValidIPv4Variable)
@@ -17,131 +17,131 @@ TEST(IPv4Suite, ValidIPv4Variable)
     char s[] = "2.3.4.5";
     ipv4_t ip = read_ipv4(s);
     EXPECT_EQ(ip.ip, 33752069);
-    EXPECT_EQ(ip.mask, 32);
+    EXPECT_EQ(ip.ps, 32);
 }
 
 TEST(IPv4Suite, ValidIPv4WithMask)
 {
     ipv4_t ip = read_ipv4("100.200.100.200/24");
     EXPECT_EQ(ip.ip, 1690854600);
-    EXPECT_EQ(ip.mask, 24);
+    EXPECT_EQ(ip.ps, 24);
 }
 
 TEST(IPv4Suite, InvalidIPv4BlockTooLarge)
 {
     ipv4_t ip = read_ipv4("100.200.100.300/24");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4TooFewBlocks)
 {
     ipv4_t ip = read_ipv4("100.200.100");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4TooManyBlocks)
 {
     ipv4_t ip = read_ipv4("100.200.100.200.100");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4InvalidMask)
 {
     ipv4_t ip = read_ipv4("100.200.100.200/40");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4MissingMaskValue)
 {
     ipv4_t ip = read_ipv4("100.200.100.200/");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4MaskIs0)
 {
     ipv4_t ip = read_ipv4("100.200.100.200/0");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4EmptyFirstBlock)
 {
     ipv4_t ip = read_ipv4(".200.100.200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4EmptySecondBlock)
 {
     ipv4_t ip = read_ipv4("100..100.200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4EmptyThirdBlock)
 {
     ipv4_t ip = read_ipv4("100.200..200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4EmptyFourthBlock)
 {
     ipv4_t ip = read_ipv4("100.200.100.");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4InvalidCharacter)
 {
     ipv4_t ip = read_ipv4("a.200.100.200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4AnotherInvalidCharacter)
 {
     ipv4_t ip = read_ipv4("100.200.-100.200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4InvalidSeparator)
 {
     ipv4_t ip = read_ipv4("100:200:100:200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, InvalidIPv4LeadingZeroes)
 {
     ipv4_t ip = read_ipv4("100.0200.100.200");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, ValidIPv4With0Block)
 {
     ipv4_t ip = read_ipv4("100.0.1.200");
     EXPECT_EQ(ip.ip, 1677722056);
-    EXPECT_EQ(ip.mask, 32);
+    EXPECT_EQ(ip.ps, 32);
 }
 
 TEST(IPv4Suite, InvalidIPv4InvalidCharacterInMask)
 {
     ipv4_t ip = read_ipv4("100.200.100.200/4-");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 0);
+    EXPECT_EQ(ip.ps, 0);
 }
 
 TEST(IPv4Suite, IPv4WithAllZeroes)
 {
     ipv4_t ip = read_ipv4("0.0.0.0");
     EXPECT_EQ(ip.ip, 0);
-    EXPECT_EQ(ip.mask, 32);
+    EXPECT_EQ(ip.ps, 32);
 }
